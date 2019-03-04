56168dc2616f8280da92434ea8661ed76d555a43
Carpa Vladimir <oswalth2@gmail.com>
2019-03-04 12:29:22 +0300

    pep8

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,18 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
+    if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        return None
+    if operator == 'plus':
+        return x + y
+    elif operator == 'minus':
+        return x - y
+    elif operator == 'mult':
+        return x * y
+    elif operator == 'divide':
+        if y == 0:
+            return None
+        return x / y
+    else:
+        return None
     raise NotImplementedError

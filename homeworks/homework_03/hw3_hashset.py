b9e301e5d06ea23d0766b186cbb961811eec444e
Carpa Vladimir <oswalth2@gmail.com>
2019-03-17 09:24:27 +0300

    hashset done

--- homeworks/homework_03/hw3_hashset.py
+++ homeworks/homework_03/hw3_hashset.py
@@ -2,13 +2,14 @@
 # coding: utf-8
 
 from homeworks.homework_03.hw3_hashmap import HashMap
+# from hw3_hashmap import HashMap
 
 
 class HashSet(HashMap):
+    PRESENT = 'value'
 
     def __init__(self):
-        # TODO Сделать правильно =)
-        raise NotImplementedError
+        super().__init__()
 
     def get(self, key, default_value=None):
         # TODO достаточно переопределить данный метод
@@ -18,7 +19,7 @@ class HashSet(HashMap):
 
     def put(self, key, value=None):
         # TODO метод put, нужно переопределить данный метод
-        super().put(key, value)
+        super().put(key, self.PRESENT)
 
     def intersect(self, another_hashset):
         # TODO метод, возвращающий новый HashSet
@@ -28,3 +29,12 @@ class HashSet(HashMap):
             if another_hashset.get(element):
                 intersection.put(element)
         return intersection
+
+    def values(self):
+        values = []
+        for bucket in self.buckets:
+            if bucket:
+                for element in bucket:
+                    a = element.key
+                    values.append(a)
+        return values

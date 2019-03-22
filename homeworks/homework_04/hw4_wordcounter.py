625062e01f68786cb86ac13bcdeb5ac653c1402f
oswalth <oswalth2@gmail.com>
2019-03-22 18:23:58 +0300

    WC done

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from multiprocessing import Process, Manager, Queue
+from multiprocessing import Process, Manager, Queue, Pool
 import os
 
 
-def process_file(q, contains):
-    filename = q.get()
-    with open('homeworks/homework_04/test_data/' + filename, encoding='utf-8') as f_obj:
+def process_file(path, filename, container):
+    with open(path + '/' + filename, encoding='utf-8') as f_obj:
         words = f_obj.read().split()
-    contains[filename] = len(words)
+    container[filename] = len(words)
 
 
 def word_count_inference(path_to_dir):
@@ -29,30 +27,17 @@ def word_count_inference(path_to_dir):
     except FileNotFoundError:
         print("Directory not found")
         return
-    q = Queue()
-    for f in files:
-        q.put(f)
+
+    container = m.dict()
     tasks = []
-    contains = m.dict()
-    for _ in range(3):
-        task = Process(target=process_file(q, contains))
+    for file in files:
+        task = Process(
+            target=process_file, args=(
+                path_to_dir, file, container))
         tasks.append(task)
         task.start()
     for task in tasks:
         task.join()
-    contains['total'] = sum(contains.values())
-
-    return contains
-
-#result = process_file('homeworks/homework_04/1.txt')
-result = word_count_inference('homeworks/homework_04/test_data')
-"""
-q = Queue()
-ans = []
-files = ['file1', 'file2', 'file3']
-for f in files:
-    q.put(f)
-for i in range(len(files)):
-    ans.append(q.get())
-print(ans)
-"""
+    container['total'] = sum(container.values())
+    print(container)
+    return container

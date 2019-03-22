e94f34b82a28ef95f22911d989befa6ba0e7461c
Carpa Vladimir <oswalth2@gmail.com>
2019-03-22 16:44:34 +0300

    pool added

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 from multiprocessing import Process, Manager, Queue, Pool
 import os
 
 
-def process_file(path, filename, container):
-    with open(path + '/' + filename, encoding='utf-8') as f_obj:
+def process_file(q_files, q_objects):
+    filename = q_files.get()
+    with open('homeworks/homework_04/test_data/' + filename, encoding='utf-8') as f_obj:
         words = f_obj.read().split()
-    container[filename] = len(words)
+    q_objects.put((filename, len(words)))
 
 
 def word_count_inference(path_to_dir):
@@ -27,17 +29,21 @@ def word_count_inference(path_to_dir):
     except FileNotFoundError:
         print("Directory not found")
         return
-
-    container = m.dict()
-    tasks = []
-    for file in files:
-        task = Process(
-            target=process_file, args=(
-                path_to_dir, file, container))
-        tasks.append(task)
-        task.start()
-    for task in tasks:
-        task.join()
+    q_files = m.Queue()
+    q_objects = m.Queue()
+    for f in files:
+        q_files.put(f)
+    container = dict()
+    with Pool(3) as p:
+        for _ in range(q_files.qsize()):
+            p.apply(process_file, (q_files, q_objects))
+    for f in files:
+        tmp = q_objects.get()
+        container[tmp[0]] = tmp[1]
     container['total'] = sum(container.values())
-    print(container)
     return container
+
+
+if __name__ == "__main__":
+    result = word_count_inference('homeworks/homework_04/test_data')
+    print(result)

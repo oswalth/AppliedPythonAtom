d070d4da1b96228affecac5919c2e7fb574171d7
Carpa Vladimir <oswalth2@gmail.com>
2019-03-22 16:29:38 +0300

    done with pools

--- homeworks/homework_04/hw4_wordcounter.py
+++ homeworks/homework_04/hw4_wordcounter.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from multiprocessing import Process, Manager, Queue
+from multiprocessing import Process, Manager, Queue, Pool
 import os
 
 
-def process_file(q, contains):
-    filename = q.get()
+def process_file(q_files, q_objects):
+    filename = q_files.get()
     with open('homeworks/homework_04/test_data/' + filename, encoding='utf-8') as f_obj:
         words = f_obj.read().split()
-    contains[filename] = len(words)
+    q_objects.put((filename, len(words)))
 
 
 def word_count_inference(path_to_dir):
@@ -29,30 +29,21 @@ def word_count_inference(path_to_dir):
     except FileNotFoundError:
         print("Directory not found")
         return
-    q = Queue()
+    q_files = m.Queue()
+    q_objects = m.Queue()
     for f in files:
-        q.put(f)
-    tasks = []
-    contains = m.dict()
-    for _ in range(3):
-        task = Process(target=process_file(q, contains))
-        tasks.append(task)
-        task.start()
-    for task in tasks:
-        task.join()
-    contains['total'] = sum(contains.values())
+        q_files.put(f)
+    container = dict()
+    with Pool(3) as p:
+        for _ in range(q_files.qsize()):
+            p.apply(process_file, (q_files, q_objects))
+    for f in files:
+        tmp = q_objects.get()
+        container[tmp[0]] = tmp[1]
+    container['total'] = sum(container.values())
+    return container
 
-    return contains
 
-#result = process_file('homeworks/homework_04/1.txt')
-result = word_count_inference('homeworks/homework_04/test_data')
-"""
-q = Queue()
-ans = []
-files = ['file1', 'file2', 'file3']
-for f in files:
-    q.put(f)
-for i in range(len(files)):
-    ans.append(q.get())
-print(ans)
-"""
+if __name__ == "__main__":
+    result = word_count_inference('homeworks/homework_04/test_data')
+    print(result)

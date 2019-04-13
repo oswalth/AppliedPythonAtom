1307aceff4fde3d68bb185eda3c33fbb37f028fe
OEM Configuration (temporary user) <oswalth2@gmail.com>
2019-04-13 11:28:06 +0300

    to pc

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
+import numpy as np
+import pandas as pd
 
 
 class LinearRegression:
@@ -9,7 +11,9 @@ class LinearRegression:
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        pass
+        self.lambda_coef = lambda_coef
+        self.regulatization = regulatization
+        self.alpha = alpha
 
     def fit(self, X_train, y_train):
         """
@@ -18,19 +22,61 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        self.X = (X_train - np.mean(X_train)) / np.std(X_train)
+        self.X_ext = np.vstack([np.ones(self.X.shape[0]), self.X])
+        self.X_ext = self.X_ext.T
+        self.Y = y_train
+        self.w = np.zeros(self.X_ext.shape[1])
+        # в случае тестируемого примера ответы примерно -224 и 5.9 соотв
+        # поэтому ставлю достаточно близкие к ним значения
+        self.w[0] = -200
+        self.w[1] = 4
 
+        accuracy = 1e-5
+        err = 0.1
+        iter_lim = 100
+        cnt = 0
+        while err > accuracy:
+            prev = np.copy(self.w)
+            self._gradient_descent()
+            err = np.sum(np.abs(self.w - prev))
+            cnt += 1
+        self.w.shape = (2, 1)
+        MSE = 1 / self.Y.shape[0] * np.sum((self.Y - self.predict(self.X)))
+        print(MSE, cnt)
+
+    def _gradient_descent(self):
+        step = 0.01
+        Y_predicted = self.predict(self.X)
+        self.w -= step * (-2 / self.X.shape[0]) * self.X_ext.T.dot((self.Y - Y_predicted))
+    
     def predict(self, X_test):
         """
         Predict using model.
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        X_test = (X_test - np.mean(X_test)) / np.std(X_test)
+        X_test = np.vstack([np.ones(X_test.shape[0]), X_test])
+        y_test = np.dot(X_test.T, self.w)
+        return y_test
+
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        return self.w
+        
+
+
+
+df = pd.read_csv('~/atom/AppliedPythonAtom/homeworks/homework_06_ml/weight-height.csv')
+wh_dataset = df.loc[df.Gender=='Male', ['Height', 'Weight']].values
+X = wh_dataset[:, 0]
+y = wh_dataset[:, 1]
+model = LinearRegression()
+model.fit(X, y)
+model.get_weights()
+print(model.predict(X))
--- homeworks/homework_06_ml/metrics.py
+++ homeworks/homework_06_ml/metrics.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
+import pandas as pd
 import numpy as np
 
 
@@ -12,7 +11,8 @@ def mse(y_true, y_hat, derivative=False):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    n = y.shape[0]
+    return 1 / n * np.sum((y_true - y_hat) ** 2)
 
 
 def mae(y_true, y_hat):
@@ -22,7 +22,8 @@ def mae(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    n = y.shape[0]
+    return 1 / n * np.sum(np.abs(y_true - y_hat))
 
 
 def r2_score(y_true, y_hat):
@@ -32,4 +33,17 @@ def r2_score(y_true, y_hat):
     :param y_hat: vector of estimated target values
     :return: loss
     """
-    pass
+    RSS = (np.sum(y_true - y_hat) ** 2)
+    TSS = (np.sum(y_true - np.average(y_true))) 
+    return 1 -  RSS/TSS 
+
+
+df = pd.read_csv('./weight-height.csv')
+wh_dataset = df.loc[df.Gender=='Male', ['Height', 'Weight']].values
+X = wh_dataset[:, 0]
+y = wh_dataset[:, 1]
+X = np.vstack([np.ones(X.shape), X])
+X = X.T
+w = (np.linalg.inv((X.T @ X)) @ X.T) @ y
+w = w.flatten()
+y_hat = (X @ w).reshape(-1)

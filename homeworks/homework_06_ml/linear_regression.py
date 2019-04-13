b18f4e630d877d36c6913ad62e8a06f809773864
oswalth <oswalth2@gmail.com>
2019-04-13 13:45:12 +0300

    metrics + grad_desc

--- homeworks/homework_06_ml/linear_regression.py
+++ homeworks/homework_06_ml/linear_regression.py
@@ -12,7 +12,8 @@ from matplotlib import pyplot as plt
 
 
 class LinearRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(self, lambda_coef=0.01, regulatization=None, alpha=0.5, accuracy=1e-7,
+        iter_lim=100000):
         """
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
@@ -21,6 +22,8 @@ class LinearRegression:
         self.lambda_coef = lambda_coef
         self.regulatization = regulatization
         self.alpha = alpha
+        self.accuracy = 1e-7
+        self.iter_lim = 100000
 
     def fit(self, X_train, y_train):
         """
@@ -29,18 +32,16 @@ class LinearRegression:
         :param y_train: target values for training data
         :return: None
         """
-        self.X = (X_train - np.mean(X_train)) / np.std(X_train)
+        self.mean_ = np.mean(X_train)
+        self.std_ = np.std(X_train)
+        self.X = (X_train - self.mean_) / self.std_
         self.X_ext = np.hstack([np.ones((self.X.shape[0], 1)), self.X])
-        self.Y = y_train
-        self.Y.shape = (self.Y.shape[0], 1)
+        self.Y = y_train[:, np.newaxis]
         self.n, self.k = self.X_ext.shape
-        self.w = np.random.randn(self.k) / np.sqrt(self.k)
-        self.w.shape = (self.w.shape[0], 1)
+        self.w = (np.random.randn(self.k) / np.sqrt(self.k))[:, np.newaxis]
 
-        accuracy = 1e-7
-        iter_lim = 100000
-        errs = np.zeros(iter_lim)
-        for i in range(iter_lim):
+        errs = np.zeros(self.iter_lim)
+        for i in range(self.iter_lim):
             if self.regulatization == 'L1':
                 fine = self.alpha * np.ones((self.k, 1)) / 2
             elif self.regulatization == "L2":
@@ -50,16 +51,14 @@ class LinearRegression:
             self._gradient_descent(fine)
             errs[i] = mse(self.Y, self.predict(X_train))
             err = np.abs(errs[i] - errs[i - 1])
-            if i and err < accuracy:
+            if i and err < self.accuracy:
                 break
         self.coef_ = self.w[1:]
         self.intercept_ = self.w[0]
-        print(err)
 
     def _gradient_descent(self, fine):
-        step = 0.01
         Y_predicted = self.predict(self.X)
-        self.w -= step * (2 / self.n) * \
+        self.w -= self.lambda_coef * (2 / self.n) * \
             (self.X_ext.T.dot((Y_predicted - self.Y)) + fine)
 
     def predict(self, X_test):
@@ -68,7 +67,7 @@ class LinearRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        X_test = (X_test - np.mean(X_test)) / np.std(X_test)
+        X_test = (X_test - self.mean_) / self.std_
         X_test = np.hstack([np.ones((X_test.shape[0], 1)), X_test])
         y_test = np.dot(X_test, self.w)
         return y_test

09364f91efacef3fa21466bcbddb4005e21beeb3
oswalth <oswalth2@gmail.com>
2019-04-15 23:52:43 +0300

    pep

--- homeworks/homework_06_ml/vis.py
+++ homeworks/homework_06_ml/vis.py
@@ -13,15 +13,15 @@ class SequantialLearning:
         self.V = 0
         self.v = np.zeros((F.shape[0], 1))
         self.T = np.zeros((F.shape[0], F.shape[0]))
-    
+
     def _get_F(self, X):
         z = symbols('z')
         res = np.array([])
         for el in self.F:
-            if isinstance(el, (int,float)):
+            if isinstance(el, (int, float)):
                 res = np.append(res, el)
                 continue
-            res = np.append(res, float(el.subs(z,X)))
+            res = np.append(res, float(el.subs(z, X)))
         res = res[:, np.newaxis]
         res = np.reshape(res, (res.shape[1], res.shape[0]))
         return res
@@ -37,34 +37,86 @@ class SequantialLearning:
         self.v += vi
         self.T += Ti
         self.cnt += 1
-        
+
     def predict(self, x_test):
         F_num = self._get_F(x_test)
         return F_num.dot(np.linalg.inv(self.T)).dot(self.v)
 
+    def get_var(self, x, sigma):
+        F_num = self._get_F(x)
+        return (
+            np.square(sigma) *
+            F_num).dot(
+            np.linalg.inv(
+                self.T)).dot(
+                F_num.T)
 
-sigma = 2
+    def get_var_hat(self, x, sigma):
+        F_num = self._get_F(x)
+        numerator = self.V - self.v.T.dot(np.linalg.inv(self.T)).dot(self.v)
+        denominator = self.cnt - F_num.size
+        return numerator / denominator * \
+            F_num.dot(np.linalg.inv(self.T)).dot(F_num.T)
+
+
+names = ['Реальные Y',
+         'Предсказанные',
+         'Реальная функция',
+         'Отклоенение Y',
+         'Отклоенение Y',
+         'Оценка отклонения',
+         'Оценка отклонения', ]
+
+
+def visualise(x, y, y_hat, var_y, var_hat_y, iter_num):
+    plt.plot(x, y, 'r.')
+    sorted_vals = np.hstack(
+        (x[:, np.newaxis], y[:, np.newaxis], y_hat, var_y, var_hat_y))
+
+    sorted_vals = np.array(sorted(sorted_vals, key=lambda x: x[0]))
+
+    x, y, y_hat = sorted_vals[:, 0], sorted_vals[:, 1], sorted_vals[:, 2]
+    var_y, var_hat_y = sorted_vals[:, 3], sorted_vals[:, 4]
+    plt.plot(x, y_hat, 'b-')
+
+    plt.plot(x, 1 + 2 * x + 0.5 * x ** 3 - 6.58 * x ** 5, 'y-')
+    plt.plot(x, y_hat + var_y, 'b--')
+    plt.plot(x, y_hat - var_y, 'b--')
+    plt.plot(x, y_hat + var_hat_y, 'g--')
+    plt.plot(x, y_hat - var_hat_y, 'g--')
+    plt.title(f'Аппроксимация функции при {i} точках')
+    plt.legend(names)
+    plt.show()
+
+
+np.random.seed(42)
+points = [10, 50, 100, 500, 999]
+sigma = 10
 x = np.linspace(-10, 10, 1000)
-y = 1 + 2*x + 3*x ** 2 - x ** 3 + np.random.normal(scale=sigma)
+np.random.shuffle(x)
+y = 1 + 2 * x + 0.5 * x ** 3 - 6.58 * \
+    x ** 5 + np.random.randn(x.shape[0]) * sigma
 z = symbols('z')
-F = np.array([1, z, z**2,z**3])
+F = np.array([1, z, z**3, z**5])
 regr = SequantialLearning(F)
-x_to_plot = np.zeros_like(x)
-x_hat_to_plot = []
-y_to_plot = np.zeros_like(y)
-y_hat_to_plot = np.zeros_like(y)
+x_to_plot = np.array([])
 for i in range(len(x)):
     regr.fit(x[i], y[i])
-    x_to_plot[i] = x[i]
-    y_to_plot[i] = y[i]
-    if i and i % 9 == 0:
-        x_hat_to_plot.append(x[i])
-        y_hat_to_plot[i] = regr.predict(x[i])
-    
-y_hat_to_plot = y_hat_to_plot[y_hat_to_plot != 0]
-plt.plot(x_to_plot, y_to_plot, '-r')
-plt.plot(x_hat_to_plot, y_hat_to_plot, '--b')
-plt.ylim((-20,20))
-plt.show()
-res = regr.predict(5)
-print(res)
+    x_to_plot = np.append(x_to_plot, x[i])
+    # print(i)
+    if i == points[0]:
+        # находим предсказание в каждой точке
+        y_hat_to_plot = np.array([regr.predict(el) for el in x_to_plot])
+        y_hat_to_plot = np.squeeze(y_hat_to_plot, axis=2)
+        # считаем отклонения
+        var_y_hat_to_plot = np.array(
+            [regr.get_var(el, sigma) for el in x_to_plot])
+        var_y_hat_to_plot = np.squeeze(var_y_hat_to_plot, axis=2)
+
+        var_hat_y_hat_to_plot = np.array(
+            [regr.get_var_hat(el, sigma) for el in x_to_plot])
+        var_hat_y_hat_to_plot = np.squeeze(var_hat_y_hat_to_plot, axis=2)
+        # рисуем
+        visualise(x_to_plot, y[:i + 1], y_hat_to_plot,
+                  var_y_hat_to_plot, var_hat_y_hat_to_plot, i)
+        points.pop(0)

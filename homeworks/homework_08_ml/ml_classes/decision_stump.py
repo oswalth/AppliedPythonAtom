338bab1b23fd13aa12d79bc19760bda3a3bf69be
oswalth <oswalth2@gmail.com>
2019-04-29 22:53:48 +0300

    knn and stump

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+from sklearn.metrics import mean_squared_error as mse
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
 
 
 class DecisionStumpRegressor:
@@ -13,7 +16,7 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        self.th = 0
 
     def fit(self, X, y):
         '''
@@ -22,7 +25,28 @@ class DecisionStumpRegressor:
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        self.X = X
+        self.y = y
+        self.n = self.X.shape[0]
+        self.get_best_split()
+
+    def get_best_split(self):
+        sorted_values = np.array(sorted(self.X[:, 0]))
+        best_mse = np.inf
+        for idx in range(len(sorted_values) - 1):
+            self.th = self.X[idx, 0]
+            right = sorted_values[sorted_values > self.th]
+            left = sorted_values[sorted_values <= self.th]
+            left_mse = len(left) / self.n * mse(self.y[:(idx + 1)], np.array(
+                [np.average(self.y[:(idx + 1)])] * (idx + 1)))
+            right_mse = len(right) / self.n * mse(self.y[idx:], np.array(
+                [np.average(self.y[idx:])] * ((self.n - idx) or 1)))
+            current = (left_mse + right_mse)
+            if current <= best_mse:
+                best_mse = current
+                self.best_idx = idx
+
+        self.th = self.X[self.best_idx, 0]
 
     def predict(self, X):
         '''
@@ -30,4 +54,22 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        if X[0] <= self.th:
+            return(np.average(self.y[:self.best_idx]))
+        else:
+            return(np.average(self.y[self.best_idx:]))
+
+
+X = np.arange(1, 100, 1)[:, np.newaxis]
+y = (np.cos(X))
+clf = DecisionStumpRegressor()
+clf.fit(X, y)
+ok = clf.predict([1])
+print(ok)
+print('ok')
+
+clf1 = DecisionTreeRegressor(max_depth=1)
+clf1.fit(X, y)
+ok = clf1.predict(np.array([[1]]))
+print(ok)
+print('ok')
--- homeworks/homework_08_ml/ml_classes/knns.py
+++ homeworks/homework_08_ml/ml_classes/knns.py
@@ -6,37 +6,6 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-class KNN:
-    """
-    simple KNN classifier
-    """
-    
-    def __init__(self, n):
-        self.n = n
-        
-    def fit(self, X, y):
-        self.X = X
-        self.y = y
-    
-    def predict(self, X):
-        y = []
-        assert len(X.shape) == 2
-        for (h, w) in X:
-            ### Посчитаем расстояние от всех элементов в тренировочной выборке
-            # до текущего примера -> результат - вектор размерности трейна
-            d = np.sqrt((h - self.X[:, 0]) ** 2 + (w - self.X[:, 1]) ** 2)
-            ### Возьмем индексы n элементов, расстояние до которых минимально
-            ### результат -> вектор из n элементов
-            idx = np.argsort(d)[:self.n]
-            ### Посчитаем частоту меток для каждого случая 
-            ### результат -> вектор длинны 2 который покажет
-            ### сколько соседей 0-го класса, сколько соседей 1-го класса.
-            counts = np.bincount(self.y[idx])
-            ### возьмем самый часто встречаемый в соседях класс.
-            prediction = np.argmax(counts)
-            y.append(prediction)
-        return y
-
 class KNNRegressor:
     """
     Построим регрессию с помощью KNN. Классификацию писали на паре
@@ -73,28 +42,15 @@ class KNNRegressor:
             # результат -> вектор из n элементов
             idx = np.argsort(d)[:self.n]
             # TODO
-            prediction = None
+            prediction = np.sum(self.y[idx]) / self.n
             y.append(prediction)
         return y
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/rmcelreath/rethinking/master/data/Howell1.csv', sep=';')
-df = df[np.logical_and(df.age > 18, df.age < 50)] 
-df_train = df.loc[:100]
-df_test = df.loc[150:180]
-
-X_train = df_train[['height', 'weight']].values
-y_train = df_train['male'].values
-
-X_test = df_test[['height', 'weight']].values
-
-clf = KNN(n=5)
-clf.fit(X_train, y_train)
-clf.predict(X_test)
-
-X, y = make_classification(n_samples=1000, n_classes=2, n_informative=14, random_state=43)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
+X, y = make_classification(n_samples=1000, n_classes=2,
+                           n_informative=14, random_state=43)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
 
 
 clf = KNNRegressor(n=3)
